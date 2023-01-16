40d6c63ad509ae02d9fd07b7d7d8a106fc82f91e
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2023-01-16 18:29:42 -0300

    test-carousel-responsive

--- src/components/historySection/gallery/Gallery.tsx
+++ src/components/historySection/gallery/Gallery.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Carousel  from "react-responsive-carousel";
 import img1 from "../../../assets/img/carousel-1.webp";
 import img2 from "../../../assets/img/carousel-2.webp";
 import img3 from "../../../assets/img/carousel-3.webp";
@@ -8,7 +9,11 @@ const images = [{ url: img1 }, { url: img2 }, { url: img3 }, { url: img4 }];
 
 function Gallery() {
 	return (
-		<></>
+		<Carousel
+			showArrows={true}
+		>
+
+    </Carousel>
 	);
 }
 
