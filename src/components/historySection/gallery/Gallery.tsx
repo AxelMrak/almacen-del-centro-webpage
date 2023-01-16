153bd60d150435735b657b0eef26822ff7f51d72
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2023-01-16 18:20:25 -0300

    Revert test

--- src/components/historySection/gallery/Gallery.tsx
+++ src/components/historySection/gallery/Gallery.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Carousel } from "@trendyol-js/react-carousel";
 import img1 from "../../../assets/img/carousel-1.webp";
 import img2 from "../../../assets/img/carousel-2.webp";
 import img3 from "../../../assets/img/carousel-3.webp";
@@ -9,11 +8,7 @@ const images = [{ url: img1 }, { url: img2 }, { url: img3 }, { url: img4 }];
 
 function Gallery() {
 	return (
-		<Carousel>
-			{images.map((img, index) => (
-				<img src={img.url} key={index} />
-			))}
-		</Carousel>
+		<></>
 	);
 }
 
