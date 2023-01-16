81227de347d8a70b3be8298aa1951e8d6f2b1c39
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2023-01-16 18:32:04 -0300

    test carousel 2

--- src/components/historySection/gallery/Gallery.tsx
+++ src/components/historySection/gallery/Gallery.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Carousel  from "react-responsive-carousel";
+import Carousel from "react-responsive-carousel";
 import img1 from "../../../assets/img/carousel-1.webp";
 import img2 from "../../../assets/img/carousel-2.webp";
 import img3 from "../../../assets/img/carousel-3.webp";
@@ -9,11 +9,13 @@ const images = [{ url: img1 }, { url: img2 }, { url: img3 }, { url: img4 }];
 
 function Gallery() {
 	return (
-		<Carousel
-			showArrows={true}
-		>
-
-    </Carousel>
+		<Carousel showArrows={true}>
+			{images.map((img, index) => {
+				<div key={index}>
+					<img src={img.url} />
+				</div>;
+			})}
+		</Carousel>
 	);
 }
 
