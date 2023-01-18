57d8bd55d065e1a0a2ef890e67f5eeb4f5542b74
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2023-01-18 17:54:10 -0300

    correct center in Map and added marker

--- src/components/locationSection/map/Map.jsx
+++ src/components/locationSection/map/Map.jsx
@@ -6,21 +6,21 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const props = {
 	center: {
-		lat: 10.99835602,
-		lng: 77.01502627,
+		lat: 33.6914754,
+		lng: 65.4678895,
 	},
 	zoom: 11,
 };
 
 function Map() {
 	return (
-		<div style={{ height: "100vh", width: "100%" }}>
+		<div style={{ height: "90vh", width: "60%" }}>
 			<GoogleMapReact
 				bootstrapURLKeys={{ key: "AIzaSyDovaGlL3mNx1ebXDpqh4sCJB35z8RKFdo" }}
 				defaultCenter={props.center}
 				defaultZoom={props.zoom}
 			>
-				<AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+				<AnyReactComponent lat={33.6914754} lng={65.4678895} text="Almacén del centro" />
 			</GoogleMapReact>
 		</div>
 	);
