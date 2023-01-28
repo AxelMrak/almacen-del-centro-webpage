c6c9e094b3b3fa991490404034ab0ae7066dfce2
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2023-01-28 11:45:49 -0300

    Lazy load added

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,16 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NotFoundPage from "../../pages/404/NotFoundPage";
-import ContactPage from "../../pages/contactPage/ContactPage";
-import HistoryPage from "../../pages/history/HistoryPage";
-import Homepage from "../../pages/home/Homepage";
-import ProductsPage from "../../pages/products/ProductsPage";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import { Spinner } from "../others/Spinner";
 import WhatsappGlobal from "../others/WhatsappGlobal";
 import "./App.css";
 
+const Homepage = lazy(() => import("../../pages/home/Homepage"));
+const HistoryPage = lazy(() => import("../../pages/history/HistoryPage"));
+const ProductsPage = lazy(() => import("../../pages/products/ProductsPage"));
+const ContactPage = lazy(() => import("../../pages/contactPage/ContactPage"));
+const NotFoundPage = lazy(() => import("../../pages/404/NotFoundPage"));
 /**
  * * Main Component who contains all app
  * ? Shows header, page in route and footer
@@ -16,25 +18,27 @@ import "./App.css";
  */
 function App() {
 	return (
-		<Router>
-			<div className="App">
-				<Header />
-			</div>
-			<Routes>
-				{/*  HomePage */}
-				<Route path="/" element={<Homepage />} />
-				{/* Historia */}
-				<Route path="/historia" element={<HistoryPage />} />
-				{/* Productos */}
-				<Route path="/productos" element={<ProductsPage />} />
-				{/* Ubicacion y contacto */}
-				<Route path="/contacto" element={<ContactPage />} />
-				{/* 404 */}
-				<Route path="*" element={<NotFoundPage />} />
-			</Routes>
-			<WhatsappGlobal />
-			<Footer />
-		</Router>
+		<div className="App">
+			<Router>
+				<Suspense fallback={<Spinner />}>
+					<Header />
+					<Routes>
+						{/*  HomePage */}
+						<Route path="/" element={<Homepage />} />
+						{/* Historia */}
+						<Route path="/historia" element={<HistoryPage />} />
+						{/* Productos */}
+						<Route path="/productos" element={<ProductsPage />} />
+						{/* Ubicacion y contacto */}
+						<Route path="/contacto" element={<ContactPage />} />
+						{/* 404 */}
+						<Route path="*" element={<NotFoundPage />} />
+					</Routes>
+					<WhatsappGlobal />
+					<Footer />
+				</Suspense>
+			</Router>
+		</div>
 	);
 }
 
